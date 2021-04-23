b29d920e5e8eda225a3622ea7bf1d1f2bed85411
jcpleitez95 <jcpleitez@outlook.com>
2021-04-23 16:10:52 -0400

    Added Banner Image

---
+++ personal_website/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header className="banner">
+            <div className="header-image">
+                <img className="banner-image" src="https://thumbs.dreamstime.com/b/internet-header-3219797.jpg" alt="banner"/>
+            </div>
+        </header>
+    )
+}
